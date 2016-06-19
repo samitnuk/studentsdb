cfbf8a1a1b19fecf405bf772c010b4af0797dc1b
Basil Dubyk <samitnuk.ua@gmail.com>
2016-06-19 20:43:09 +0300

    add views for tests

--- students/views.py
+++ students/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def test(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+# Views for Students
+
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+
+def students_add(request):
+    return HttpResponse('<h1>Students Add Form</h1>')
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
+# Views for Groups
+
+def groups_list(request):
+    return render(request, 'students/groups_list.html', {})
+
+def groups_add(request):
+    return HttpResponse('<h1>Group Add Form</h1>')
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Groups %s</h1>' % gid)
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
