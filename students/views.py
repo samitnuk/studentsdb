d5d907fa6be903df8a703c9cecc0e92bdcd8d893
samitnuk <samitnuk.ua@gmail.com>
2016-06-20 18:46:50 +0300

    test dymamic in template

--- students/views.py
+++ students/views.py
@@ -1,10 +1,30 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for Students
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': 'Віталій',
+         'last_name': 'Подоба',
+         'ticket': 235,
+         'image': 'img/image1.jpg'},
+        {'id': 2,
+         'first_name': 'Андрій',
+         'last_name': 'Корост',
+         'ticket': 2435,
+         'image': 'img/image2.jpg'},
+         {'id': 3,
+         'first_name': 'Іван',
+         'last_name': 'Драган',
+         'ticket': 7235,
+         'image': 'img/image3.jpg'},
+    )
+    return render(request, 'students/students_list.html',
+        {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Students Add Form</h1>')
