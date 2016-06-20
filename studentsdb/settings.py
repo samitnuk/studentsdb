57e4710635ea4a2e709b99bffe2edec83a5a7b2f
samitnuk <samitnuk.ua@gmail.com>
2016-06-20 23:09:47 +0300

    add our own context processor

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'studentsdb.context_processor.students_proc',
             ],
         },
     },
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PORTAL_URL = 'http://127.0.0.1:8000'
