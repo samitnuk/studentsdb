d38f1c9c3adfbc62dce832e6bd1835a256ee98c9
samitnuk <samitnuk.ua@gmail.com>
2016-06-22 23:36:48 +0300

    add Student model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    """Student Model"""
+
+    
+
+
+    first_name = models.CharField(
+        max_lenght=256,
+        blank=False,
+        verbose_name='Ім’я')
+
+    last_name = models.CharField(
+        max_lenght=256,
+        blank=False,
+        verbose_name='Прізвище')
+
+    middle_name = models.CharField(
+        max_lenght=256,
+        blank=True,
+        verbose_name='По-батькові',
+        default='')
+
+    birthday = models.DateField(
+        blank=False,
+        verbose_name='Дата народження',
+        null=True)
+
+    photo = models.ImageField(
+        blank=True,
+        verbose_name='Фото',
+        null=True)
+
+    ticket = models.CharField(
+        max_lenght=256,
+        blank=False,
+        verbose_name='Білет')
+
+    notes = models.TextField(
+        blank=True,
+        verbose_name='Додаткові нотатки')
