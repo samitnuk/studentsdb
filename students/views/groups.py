c3fea57492507c468ccbb02ecb2b62fd6eb0318f
samitnuk <samitnuk.ua@gmail.com>
2016-06-25 16:21:57 +0300

    update groups list view

--- students/views/groups.py
+++ students/views/groups.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from ..models import Group
 
 
 def groups_list(request):
-    return render(request, 'students/groups_list.html', {})
+    groups = Group.objects.all()
+
+    # try to order groups list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('id', 'title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    # order by title if page loads first time
+    else:
+        groups = groups.order_by('title')
+
+    # paginate groups
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page
+        groups = paginator.page(1)
+    except EmptyPage:
+        # if page is oout of range (e.g. 9999), deliver
+        # last page of results
+        groups = paginator.page(paginator.num_pages)
+
+    return render(request, 'students/groups_list.html',
+        {'groups_db': groups})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
