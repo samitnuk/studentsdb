f6310821e095df85ea40bb1523aa3d03614bb09d
samitnuk <samitnuk.ua@gmail.com>
2016-07-09 22:35:19 +0300

    add ability to work with datebase

--- students/views/journal.py
+++ students/views/journal.py
@@ -4,6 +4,7 @@ from calendar import monthrange, weekday, day_abbr
 
 from django.core.urlresolvers import reverse
 from django.views.generic.base import TemplateView
+from django.http import JsonResponse
 
 from ..models import Student, MonthJournal
 from ..utils import paginate
@@ -81,10 +82,29 @@ class JournalView(TemplateView):
                 'update_url': update_url,
             })
 
-
         # apply paginattion, 10 students per page
         context = paginate(students, 3, self.request, context,
-                               var_name='students')
+                           var_name='students')
 
         # повертаємо оновлений словник із даними
         return context
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+
+        # prepare student, dates and presence data
+        current_date = datetime.strptime(data['date'], '%Y-%m-%d').date()
+        month = date(current_date.year, current_date.month, 1)
+        present = data['present'] and True or False
+        student = Student.objects.get(pk=data['pk'])
+
+        # get or create journal object for given student and month
+        journal = MonthJournal.objects.get_or_create(student=student,
+                                                     date=month)[0]
+
+        # set new presence on journal for given student and save result
+        setattr(journal, 'present_day%d' % current_date.day, present)
+        journal.save()
+
+        # return success status
+        return JsonResponse({'status': 'success'})
